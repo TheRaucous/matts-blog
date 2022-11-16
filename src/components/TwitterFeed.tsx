479fc21b17b33cbc4eb25efd3f529edaea5dd687
TheRaucous <88458240+TheRaucous@users.noreply.github.com>
2022-11-16 10:36:58 -0600

    Removed unnecessary files.

--- src/components/TwitterFeed.tsx
+++
@@ -1,5 +0,0 @@
-export default function TwitterFeed() {
-  return (
-    <div>TwitterFeed</div>
-  )
-}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,28 +3,13 @@ import { getValidatedContent } from '../lib/MDXContent';
 import PostsFeed from '../components/PostsFeed';
 // import TwitterFeed from '../components/TwitterFeed';
 // import GridEffect from '../components/gridEffect/GridEffect';
-import Hero from '../components/Hero';
 
 export default function Home({ posts }) {
-  const HeroElem = () => <Hero />;
   return (
     <>
       <Head>
         <title>Home - MatMac</title>
       </Head>
-      {/* <div className="relative">
-        <HeroElem />
-        <div className="pointer-events-none absolute inset-0 z-10 flex items-center justify-center">
-          <div className="pointer-events-auto h-fit w-fit bg-c-bg-01/75 shadow-md shadow-black/50 backdrop-blur-sm md:flex">
-            <div className="flex h-64 w-64 items-center border md:h-80 md:w-80">
-              <h1 className="w-full text-center text-3xl">Stuff goes here</h1>
-            </div>
-            <div className="flex h-64 w-64 items-center border md:h-80 md:w-80">
-              <h1 className="w-full text-center text-3xl">Stuff goes here</h1>
-            </div>
-          </div>
-        </div>
-      </div> */}
       <main className="mt-36">
         <h1 className="text-center text-2xl">Latest Posts</h1>
         <PostsFeed posts={posts} />
