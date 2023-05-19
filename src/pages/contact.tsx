b64f8cb3c386a345988f38f5eaa1aa964e573286
TheRaucous <88458240+TheRaucous@users.noreply.github.com>
2023-05-18 21:26:56 -0500

    Imported React into file

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import { useRef } from 'react';
+import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
 
 export default function contact() {
